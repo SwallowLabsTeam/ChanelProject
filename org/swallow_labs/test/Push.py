4eb15952d367f3b64a2a9e795a57a544c4cfa00c
Akrem <mr.sallemiakrem@gmail.com>
2016-05-25 11:38:33 +0100

    Change status capsule

--- org/swallow_labs/test/Push.py
+++ org/swallow_labs/test/Push.py
@@ -6,8 +6,5 @@ c = Client("10", "localhost:5559")
 capsule = Capsule(c.id_client)
 capsule.set_type("PAYLOAD")
 capsule.set_payload({'nom': 'seif', 'prenom': 'gamoudi'})
-capsule.set_status_capsule("NOTSENT")
-
-
 capsule.set_id_receiver("20")
 c.push(capsule)
