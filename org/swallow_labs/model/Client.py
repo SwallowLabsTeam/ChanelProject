23102aae6770c96390770a1715b071c6a374ba3d
salmen <salmen-karray@hotmail.fr>
2016-05-31 17:13:32 +0200

    Merge branch 'dev' of https://github.com/SwallowLabsTeam/ChanelProject
    Conflicts:
    	org/swallow_labs/model/Broker.py

--- org/swallow_labs/model/Client.py
+++ org/swallow_labs/model/Client.py
@@ -1,13 +1,12 @@
-import zmq
-import socket
-import json
-import time
-from contextlib import closing
-from org.swallow_labs.model.Capsule import Capsule
-from org.swallow_labs.tool.CapsuleType import CapsuleType
+from org.swallow_labs.model.SocketClient import SocketClient
+import queue as Q
+
+
+
 
 
 class Client:
+
     """
     Class creating a client object:
 
@@ -22,98 +21,104 @@ class Client:
     Return a client
 
     @param id_client : a client id
-    @param address : ip address of the host the client is connecting to
-    @param port: port of the host the client is connecting to
+    @param list_address : List of ip address and ports of the host the client is connecting to
+    @param sock_list: list of cnx socket used by the Client
+    @param nbr_broker: Number of Brokers where the client is connecting to
+    @param pull_list: List of message pulled by the client
+
     @ivar self.id_client : a client id
-    @ivar self.address : ip address:server adresse the client is connecting to
-    @ivar self.port : port of the host the client is connecting to
-    @ivar self.socket: the socket enabling the connection
+    @ivar list_address : List of ip address and ports of the host the client is connecting to
+    @ivar sock_list: list of cnx socket used by the Client
+    @ivar nbr_broker: Number of Brokers where the client is connecting to
+    @ivar pull_list: List of message pulled by the client
 
     """
+    cpt = 0
 
-    def __init__(self, id_client, address, port):
+    def __init__(self,id_client,list_address ):
+
+        self.id_client=id_client
+        self.list_address=list_address
+        self.sock_list=[]
+        self.nbr_broker=len(list_address)
+        self.pull_list=[]
+
+
+
+
+
+    def generate(self):
         """
 
+                DESCRIPTION
+                ===========
+                Method generate the Client communication stub (MultiSocket)
+         """
+        for i in range(self.nbr_broker):
+            self.sock_list.append(SocketClient(self.id_client,self.list_address[i].adr,self.list_address[i].port))
+
+    def push(self,capsule):
 
         """
-        # Initialize client
-        self.id_client = id_client
-        self.address = address
-        self.port = port
-        # Create a Context object to be able to call socket method
-        context = zmq.Context(1)
-        # Create a DEALER socket
-        self.socket = context.socket(zmq.DEALER)
-        # Assign an id to the client
-        self.socket.setsockopt(zmq.IDENTITY, bytes(self.id_client,"utf8"))
-        # Connect to the designed host
-        self.socket.connect("tcp://" + self.address + ":" + self.port)
 
-    # Method that check if the port server is open or not
+                        DESCRIPTION
+                        ===========
+                        Method sending capsule for the appropriate broker
 
-    def check_port(self, adr, pr):
+                        @param capsule : the capsule to send
         """
 
-        DESCRIPTION
-        ===========
-        Method check if the port server is open or not
+        self.sock_list[self.cpt].push(capsule)
+        Client.cpt_inc()
+        if Client.cpt == self.nbr_broker :
+            Client.cpt_zero()
 
-        @param adr : server address
-        @param pr  : server port
-
-        """
-        with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
-            if sock.connect_ex((adr, int(pr))) == 0:
-                return 1
-
-            else:
-                return 0
-
-    # Method sending a message to an other client via the broker
-    def push(self, capsule):
-        """
-
-        DESCRIPTION
-        ===========
-        Method providing a way for the client to send messages through the broker
-
-        @param capsule : the capsule to send
-
-        """
-        if self.check_port(self.address, self.port):
-
-            self.socket.send_json(json.dumps(capsule.__dict__))
-        else:
-            print("server closed")
-
-    # Method allowing the client to pull the data concerning him
     def pull(self):
+
         """
-        DESCRIPTION
-        ===========
-        Method allowing the client to pull the messages that concern him from the broker
 
-        @rtype: List
+                                DESCRIPTION
+                                ===========
+                               Method allowing the client to pull the messages that concern him from all Broker and sort them
+
+
         """
-        if self.check_port(self.address, self.port):
 
-            c = Capsule(self.id_client)
-            c.set_type(CapsuleType.READY)
-            self.socket.send_json(json.dumps(c.__dict__))
-            message_list = []
-            while True:
-                j = self.socket.recv_json()
-                p = json.dumps(j)
+        for i in range(self.nbr_broker):
+            self.pull_list=self.pull_list+self.sock_list[i].pull()
+        self.tri()
 
-                c = Capsule(j=p)
-                if c.get_type() == CapsuleType.END:
-                    break
-                else:
-                    c.set_receiving_date(time.localtime())
-                    message_list.append(c)
-                    print("Let's talk about {}".format(json.dumps(c.__dict__)))
-            return message_list
+    def tri(self):
+        """
 
-        else:
+                         DESCRIPTION
+                         ===========
+                        Method that sort the pull_list
 
-            return [None]
+
+         """
+
+
+        inter=[]
+        aux=Q.PriorityQueue()
+        for i in range(len(self.pull_list)):
+
+           aux.put((self.pull_list[i].get_priority(),i))
+
+        #self.pull_list=[]
+        while not aux.empty():
+
+
+            inter.append(self.pull_list[aux.get()[1]])
+
+        self.pull_list=inter
+
+    @staticmethod
+    def cpt_inc():
+        Client.cpt += 1
+        return Client.cpt
+
+    @staticmethod
+    def cpt_zero():
+        Client.cpt=0
+        return Client.cpt
