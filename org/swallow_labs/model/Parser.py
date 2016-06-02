8ebc30a4acd1be3cf7aecedb4a0493d63ca1390f
GamoudiSeif <gamoudi.seif@gmail.com>
2016-06-02 09:43:26 +0100

    Remove BrokerData class

--- org/swallow_labs/model/Parser.py
+++ org/swallow_labs/model/Parser.py
@@ -1,25 +1,7 @@
 import json
 from jsonschema import validate
 import ast
-
-
-class BrokerData:
-    """
-        Class creating a BrokerData object
-        G{classtree}
-        DESCRIPTION
-        ===========
-            Class containing the necessary data about the brokers to be able to initialize the clients
-
-        @param address:    The Broker's ip address
-        @param port:    port number
-
-        @type address: string
-        @type port: int
-    """
-    def __init__(self, address, port):
-        self.address = address
-        self.port = port
+from org.swallow_labs.model.BrokerData import BrokerData
 
 
 class Parser:
