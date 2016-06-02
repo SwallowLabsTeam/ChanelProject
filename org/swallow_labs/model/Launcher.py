43e00379f30a73263038ca841793aa68103522a7
salmen <salmen-karray@hotmail.fr>
2016-06-02 15:45:21 +0200

    Merge branch 'dev' of https://github.com/SwallowLabsTeam/ChanelProject
    Conflicts:
    	org/swallow_labs/model/Broker.py

--- org/swallow_labs/model/Launcher.py
+++ org/swallow_labs/model/Launcher.py
@@ -2,6 +2,7 @@ import json
 import socket
 from multiprocessing import Process
 from org.swallow_labs.model.Broker import Broker
+import time
 
 
 class Launcher:
@@ -25,6 +26,7 @@ class Launcher:
             if socket.gethostbyname(socket.gethostname()) == dict(data)['ip_add'][i]:
                 self.p[i] = Process(target=self.broker_def, args=(dict(data)['front_end'][i], dict(data)['back_end'][i]))
                 self.p[i].start()
+                time.sleep(0.01)
 
     @staticmethod
     def broker_def(front_end, back_end):
