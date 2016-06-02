df4d3615e917eb03af32a2c4fefead2468fdb46a
salmen <salmen-karray@hotmail.fr>
2016-06-02 17:25:51 +0200

    Merge branch 'dev' of https://github.com/SwallowLabsTeam/ChanelProject
    Conflicts:
    	org/swallow_labs/model/Broker.py

---
+++ org/swallow_labs/test/T001.2testParam.py
@@ -0,0 +1,18 @@
+from org.swallow_labs.model.Parser import *
+from org.swallow_labs.model.Client import *
+from org.swallow_labs.model.Launcher import *
+
+"""json_data = open('json example').read()
+data = json.loads(json_data)
+json_data = open('schema').read()
+schema = ast.literal_eval(json_data)
+validate(data, schema)  # in case the json data isn't valid this function will throw an exception.
+
+parser = Parser('client', 'json example')
+client = Client(parser.get_client_id(), parser.get_broker_list())
+print(client)
+print("client id: {}".format(client.id_client))
+for x in client.list_address:
+    print("le client est connecté sur le broker de port: {} et addresse: {}".format(x.port, x.address))"""
+
+broker_launcher = Launcher('json example')
