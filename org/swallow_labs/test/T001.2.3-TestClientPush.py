7da4af64e06531aca5a8f1fa6ce9c20be5e7c23e
salmen <salmen-karray@hotmail.fr>
2016-06-02 16:10:23 +0200

    Merge branch 'dev' of https://github.com/SwallowLabsTeam/ChanelProject
    Conflicts:
    	org/swallow_labs/model/Broker.py

--- org/swallow_labs/test/T001.2.3-TestClientPush.py
+++ org/swallow_labs/test/T001.2.3-TestClientPush.py
@@ -1,40 +1,27 @@
+'''Script to instance a client stub and push two capsule'''
 from org.swallow_labs.model.Client import Client
 from org.swallow_labs.model.Capsule import Capsule
 from org.swallow_labs.model.Parser import *
 from org.swallow_labs.tool.CapsulePriority import CapsulePriority
-
-p=Parser('client2', 'json example')
-a=p.get_client_id()
-l=p.get_broker_list()
+p = Parser('client2', 'json example')
+a = p.get_client_id()
+l = p.get_broker_list()
 print("ID client: ", a)
 for v in l:
     print("Connexion address:", v.address)
     print("Port address :", v.port)
-
 c = Client(a, l)
-
 capsule = Capsule(c.id_client)
 capsule.set_type("PAYLOAD")
-capsule.set_payload({'nom': 'seif', 'prenom': 'gamoudi'})
+capsule.set_payload({'nom': 'gammoudi', 'prenom': 'seif'})
 capsule.set_id_receiver("20")
-
 capsule.set_priority(CapsulePriority.BOOKING_MSG)
 capsule2 = Capsule(c.id_client)
 capsule2.set_type("PAYLOAD")
-capsule2.set_payload({'nom': 'aaaa', 'prenom': 'bbbb'})
+capsule2.set_payload({'nom': 'Sallemi', 'prenom': 'Akram'})
 capsule2.set_id_receiver("20")
 capsule2.set_priority(CapsulePriority.INFORMATION_DEVICE_MSG)
-# time.sleep(1)
-capsule3 = Capsule(c.id_client)
-capsule3.set_type("PAYLOAD")
-capsule3.set_payload({'nom': 'zzzzz', 'prenom': 'bbbb'})
-capsule3.set_id_receiver("20")
-capsule3.set_priority(CapsulePriority.UPDATE_MSG)
 c.generate()
 c.push(capsule)
-c.push(capsule)
 c.push(capsule2)
-c.push(capsule)
-c.push(capsule3)
-c.push(capsule)
-c.push(capsule2)
+
