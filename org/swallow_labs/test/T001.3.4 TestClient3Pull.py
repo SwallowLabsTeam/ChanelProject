185fe86f17429d1c75fbf9a108bde59b9d8c0310
GamoudiSeif <gamoudi.seif@gmail.com>
2016-06-03 15:56:50 +0100

    update

--- org/swallow_labs/test/T001.3.4 TestClient3Pull.py
+++ org/swallow_labs/test/T001.3.4 TestClient3Pull.py
@@ -6,28 +6,17 @@ from org.swallow_labs.model.Parser import *
 p = Parser('client3', 'json example')
 a = p.get_client_id()
 l = p.get_broker_list()
-print("ID client: ",a)
-for v in l:
-    print("Connexion address:", v.address)
-    print("Port address :", v.port)
-
-#Client instance (param : client_Id and list of brokers address
-c1=Client(a,l)
-
-#generate client Sockets of cnx with brokers
+c1 = Client(a, l)
 c1.generate()
+if c1.pull():
+    if len(c1.pull_list) == 0:
+        print("No Messaages")
+    else:
+        for x in c1.pull_list:
+            print("Capsule received")
 
-#Print sockets cnx port
-for h in c1.sock_list:
-    print(h.port)
 
-#pull messages
-c1.pull()
 
-#print messages (id & priority)
-print("Message List :")
-for x in c1.pull_list:
-    print("capsule id: ", int(x.get_id_capsule()))
-    print("capsule priority:", x.get_priority())
-    print("*************************************")
+
+
 
