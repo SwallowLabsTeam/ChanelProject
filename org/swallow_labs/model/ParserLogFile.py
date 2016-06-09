290110acc26ae68b777cce848c4344e514c5c17d
salmen <salmen-karray@hotmail.fr>
2016-06-09 16:11:25 +0200

    Merge branch 'dev' of https://github.com/SwallowLabsTeam/ChanelProject
    Conflicts:
    	org/swallow_labs/model/Broker.py

--- org/swallow_labs/model/ParserLogFile.py
+++ org/swallow_labs/model/ParserLogFile.py
@@ -13,7 +13,7 @@ class ParserLogFile:
         return param
 
     def get_param_client(self,id_client):
-        param = [self.data['HOST'], self.data['PORT'], self.data['LEVEL_CLIENT'], self.data['FACILITY_CLIENT'], self.data['FORMAT'], id_client]
+        param = [self.data['HOST'], self.data['PORT'], self.data['LEVEL_CLIENT'], self.data['FACILITY_CLIENT'], self.data['FORMAT'], "CLIENT"]
         return param
 
 
