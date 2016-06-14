8482e9e7dd20a2e8f2b79caae4449bfea9ca0c0a
Akrem <mr.sallemiakrem@gmail.com>
2016-06-14 13:02:27 +0100

    Client log and Capsule log

---
+++ org/swallow_labs/test/T002.1.3-TestClientLogger.py
@@ -0,0 +1,10 @@
+"Script to create 2 clients and log the operation "
+
+from org.swallow_labs.model.Client import Client
+from org.swallow_labs.model.Parser import *
+
+p = Parser('../configuration/Configuration.json', 'client')
+id_client = p.get_client_id()
+connexions = p.get_broker_list()
+client = Client(id_client, connexions)
+print("Client launched")
