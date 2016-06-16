2c667468f05fb014a7b471b87a9c34821f7f485a
GamoudiSeif <gamoudi.seif@gmail.com>
2016-06-16 11:30:17 +0100

    Review Parser

--- org/swallow_labs/model/Parser.py
+++ org/swallow_labs/model/Parser.py
@@ -1,6 +1,5 @@
 import json
-from jsonschema import validate
-from jsonschema import *
+from jsonschema import validate, ValidationError
 import ast
 from org.swallow_labs.model.BrokerData import BrokerData
 
@@ -20,62 +19,125 @@ class Parser:
         @type client_path: string
         @type broker_path: string
     """
-    def __init__(self, broker_path, client_path=None):
-        json_data = open('../test/schema').read()
-        schema = ast.literal_eval(json_data)
-        if broker_path is not None and client_path is not None:
-            json_data = open(client_path).read()
-            self.client_id = json.loads(json_data)
-            json_data = open(broker_path).read()
-            self.data = json.loads(json_data)
-            self.broker_list = []
+    __data = {}
+    __backend_broker_list = []
+    __frontend_broker_list = []
+    __broker_log_param = []
+    __client_log_param = []
+    __capsule_log_param = []
+    __snapshot_param = ''
+
+    @staticmethod
+    def read(files_path_list=['../conf/Configuration.json'], schema_path='../test/schema'):
+        schema = ast.literal_eval(open(schema_path).read())
+        for f in files_path_list:
             try:
-                validate(self.data, schema)
-            except ValidationError:
-                print("Your json file is INVALID")
-            if self.client_id >= 10:
-                for i in range(len(dict(self.data)['net_param']['ip_add'])):
-                    self.broker_list.append(BrokerData(dict(self.data)['net_param']['ip_add'][i],
-                                                       dict(self.data)['net_param']['back_end'][i]))
-            else:
-                    for i in range(len(dict(self.data)['net_param']['ip_add'])):
-                        self.broker_list.append(BrokerData(dict(self.data)['net_param']['ip_add'][i],
-                                                           dict(self.data)['net_param']['front_end'][i]))
-        else:
-            json_data = open(broker_path).read()
-            self.data = json.loads(json_data)
-            try:
-                validate(self.data, schema)
-            except ValidationError:
-                print("Your json file is INVALID")
+                Parser.__data = {**Parser.__data, **json.loads(open(f).read())}
+                print(Parser.__data)
+                if 'Configuration.json' in f:
+                    try:
+                        validate(json.loads(open(f).read()), schema)
+                    except ValidationError:
+                        pass
+            except FileNotFoundError:
+                pass
 
-    def get_param_log_broker(self):
+    @staticmethod
+    def set_all():
+        Parser.set_backend_broker_list()
+        Parser.set_frontend_broker_list()
+        Parser.set_broker_log_param()
+        Parser.set_capsule_log_param()
+        Parser.set_client_log_param()
+        Parser.set_snapshot_param()
 
-        param = [self.data['log_param']['host'], self.data['log_param']['port'], self.data['log_param']['level_broker'],
-                 self.data['log_param']['facility_broker'], self.data['log_param']['format'], "Broker"]
-        return param
+    @staticmethod
+    def set_backend_broker_list():
+        try:
+            for i in range(len(dict(Parser.__data)['net_param']['ip_add'])):
+                Parser.__backend_broker_list.append(BrokerData(dict(Parser.__data)['net_param']['ip_add'][i],
+                                                               dict(Parser.__data)['net_param']['back_end'][i]))
+        except:
+            pass
 
-    def get_param_log_client(self):
-        param = [self.data['log_param']['host'], self.data['log_param']['port'], self.data['log_param']['level_client'],
-                 self.data['log_param']['facility_client'], self.data['log_param']['format'], "Client"]
-        return param
+    @staticmethod
+    def set_frontend_broker_list():
+        try:
+            for i in range(len(dict(Parser.__data)['net_param']['ip_add'])):
+                Parser.__frontend_broker_list.append(BrokerData(dict(Parser.__data)['net_param']['ip_add'][i],
+                                                                dict(Parser.__data)['net_param']['front_end'][i]))
+        except:
+            pass
 
-    def get_param_log_capsule(self):
-        param = [self.data['log_param']['host'], self.data['log_param']['port'],
-                 self.data['log_param']['level_capsule'],
-                 self.data['log_param']['facility_capsule'],
-                 self.data['log_param']['format'], "Capsule"]
-        return param
+    @staticmethod
+    def set_broker_log_param():
+        try:
+            Parser.__broker_log_param = [Parser.__data['log_param']['host'],
+                                         Parser.__data['log_param']['port'],
+                                         Parser.__data['log_param']['level_broker'],
+                                         Parser.__data['log_param']['facility_broker'],
+                                         Parser.__data['log_param']['format'],
+                                         "Broker"]
+        except:
+            pass
 
-    def get_param_snapshot(self):
-        param = self.data['snapshot_param']['path']
-        return param
+    @staticmethod
+    def set_client_log_param():
+        try:
+            Parser.__client_log_param = [Parser.__data['log_param']['host'],
+                                         Parser.__data['log_param']['port'],
+                                         Parser.__data['log_param']['level_client'],
+                                         Parser.__data['log_param']['facility_client'],
+                                         Parser.__data['log_param']['format'],
+                                         "Client"]
+        except:
+            pass
 
-    def get_client_id(self):
-        return self.client_id
+    @staticmethod
+    def set_capsule_log_param():
+        try:
+            Parser.__capsule_log_param = [Parser.__data['log_param']['host'],
+                                          Parser.__data['log_param']['port'],
+                                          Parser.__data['log_param']['level_capsule'],
+                                          Parser.__data['log_param']['facility_capsule'],
+                                          Parser.__data['log_param']['format'],
+                                          "Capsule"]
+        except:
+            pass
 
-    def get_broker_list(self):
-        return self.broker_list
+    @staticmethod
+    def set_snapshot_param():
+        try:
+            Parser.__snapshot_param = Parser.data['snapshot_param']['path']
+        except:
+            pass
 
+    @staticmethod
+    def get_backend_broker_list():
+        return Parser.__backend_broker_list
 
+    @staticmethod
+    def get_frontend_broker_list():
+        return Parser.__frontend_broker_list
+
+    @staticmethod
+    def get_broker_log_param():
+        return Parser.__broker_log_param
+
+    @staticmethod
+    def get_client_log_param():
+        return Parser.__client_log_param
+
+    @staticmethod
+    def get_capsule_log_param():
+        return Parser.__capsule_log_param
+
+    @staticmethod
+    def get_snapshot_param():
+        return Parser.__snapshot_param
+
+if __name__ == '__main__':
+
+    Parser.set_all()
+    print(Parser.get_backend_broker_list())
 
