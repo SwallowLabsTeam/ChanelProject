950d935257c51160c269afa80e30120e97775fce
salmen <salmen-karray@hotmail.fr>
2016-11-16 16:53:46 +0100

    add tool class named CapsuleSort that contain capsule sort

--- org/swallow_labs/model/RunClient.py
+++ org/swallow_labs/model/RunClient.py
@@ -1,7 +1,7 @@
 from org.swallow_labs.model.Client import Client
 from org.swallow_labs.model.Parser import *
-from org.swallow_labs.model.Capsule import Capsule
 from multiprocessing import Process
+from org.swallow_labs.model.CapsuleProcessor import CapsuleProcessor
 import time
 class RunClient:
     """
@@ -13,28 +13,54 @@ class RunClient:
 
         @param id_client    : a client id
         @param list_address : List of ip address and ports of the host the client is connecting to
+        @param client       : the client stub
 
         @type id_client     : int
         @type list_address  : list
+        @type client        : Client
 
     """
+
     def __init__(self, id_client):
 
+        """
+
+
+        """
         self.id_client = id_client
         self.list_address = Parser.get_backend_broker_list()
-        p = Process(target=self.routine, args=(self.id_client,self.list_address))
+        # Load client param
+        self.client = Client(self.id_client, self.list_address)
+        # instantiate a Client
+        p = Process(target=self.routine)
         p.start()
         time.sleep(0.01)
+        # launch the routine method in a process
 
-    @staticmethod
-    def routine(x,y):
-         c = Client(x,y)
-         c.generate()
+
+    def routine(self):
+
+         """
+            DESCRIPTION
+        ===========
+        Method that run a client stub and pull capsule in loop
+
+         """
+         self.client.generate()
          while(True):
-             c.pull()
-             print("#####################################")
-             for x in c.pull_list:
+          # loop for pull
+             self.client.pull()
+            # client pull
+             for x in self.client.pull_list:
                  print(x)
-             print("######################################")
+                 t = CapsuleProcessor(x)
+                 # instantiate a CapsuleProcessor that will treat capsule
+                 t.treat()
+                 # treat capsule
+                 x.my_logger.log_treated_capsule(x)
+                 # log that the capsule was treated
+                 self.client.pull_list.pop(0)
+                 # pop the treated capsule from the pull_list
+             # treat pull_list capsule
              time.sleep(3)
 
