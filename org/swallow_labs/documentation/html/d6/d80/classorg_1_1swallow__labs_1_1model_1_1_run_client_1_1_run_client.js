0607a0cfedc1441239fe28930a578bd2abaf8997
root <madbelmalek@swallow-labs.com>
2016-12-23 14:56:33 +0100

    add global variable my_logger

---
+++ org/swallow_labs/documentation/html/d6/d80/classorg_1_1swallow__labs_1_1model_1_1_run_client_1_1_run_client.js
@@ -0,0 +1,7 @@
+var classorg_1_1swallow__labs_1_1model_1_1_run_client_1_1_run_client =
+[
+    [ "__init__", "d6/d80/classorg_1_1swallow__labs_1_1model_1_1_run_client_1_1_run_client.html#a81ec48b20543e3646635882056c0e719", null ],
+    [ "routine", "d6/d80/classorg_1_1swallow__labs_1_1model_1_1_run_client_1_1_run_client.html#ac0069ee35dc6fb476174bc36d9e6aa26", null ],
+    [ "id_client", "d6/d80/classorg_1_1swallow__labs_1_1model_1_1_run_client_1_1_run_client.html#a876ad6e3b9f3e77dd2862a89b6b920a3", null ],
+    [ "list_address", "d6/d80/classorg_1_1swallow__labs_1_1model_1_1_run_client_1_1_run_client.html#ad4854628e4a05a680f45b9bd47de74d2", null ]
+];
---
+++ org/swallow_labs/documentation/html/d9/da1/namespaceorg_1_1swallow__labs_1_1model.js
@@ -0,0 +1,15 @@
+var namespaceorg_1_1swallow__labs_1_1model =
+[
+    [ "Broker", "db/d4f/namespaceorg_1_1swallow__labs_1_1model_1_1_broker.html", "db/d4f/namespaceorg_1_1swallow__labs_1_1model_1_1_broker" ],
+    [ "BrokerData", "d0/df4/namespaceorg_1_1swallow__labs_1_1model_1_1_broker_data.html", "d0/df4/namespaceorg_1_1swallow__labs_1_1model_1_1_broker_data" ],
+    [ "Capsule", "d6/d87/namespaceorg_1_1swallow__labs_1_1model_1_1_capsule.html", "d6/d87/namespaceorg_1_1swallow__labs_1_1model_1_1_capsule" ],
+    [ "CapsuleACK", "db/d49/namespaceorg_1_1swallow__labs_1_1model_1_1_capsule_a_c_k.html", "db/d49/namespaceorg_1_1swallow__labs_1_1model_1_1_capsule_a_c_k" ],
+    [ "CapsuleProcessor", "d7/dd1/namespaceorg_1_1swallow__labs_1_1model_1_1_capsule_processor.html", "d7/dd1/namespaceorg_1_1swallow__labs_1_1model_1_1_capsule_processor" ],
+    [ "Client", "d6/d42/namespaceorg_1_1swallow__labs_1_1model_1_1_client.html", "d6/d42/namespaceorg_1_1swallow__labs_1_1model_1_1_client" ],
+    [ "EmergencyToolBox", "d1/d92/namespaceorg_1_1swallow__labs_1_1model_1_1_emergency_tool_box.html", "d1/d92/namespaceorg_1_1swallow__labs_1_1model_1_1_emergency_tool_box" ],
+    [ "Launcher", "d3/d51/namespaceorg_1_1swallow__labs_1_1model_1_1_launcher.html", "d3/d51/namespaceorg_1_1swallow__labs_1_1model_1_1_launcher" ],
+    [ "LdapParam", "de/d66/namespaceorg_1_1swallow__labs_1_1model_1_1_ldap_param.html", "de/d66/namespaceorg_1_1swallow__labs_1_1model_1_1_ldap_param" ],
+    [ "Parser", "d0/d7a/namespaceorg_1_1swallow__labs_1_1model_1_1_parser.html", "d0/d7a/namespaceorg_1_1swallow__labs_1_1model_1_1_parser" ],
+    [ "RunClient", "d7/d0a/namespaceorg_1_1swallow__labs_1_1model_1_1_run_client.html", "d7/d0a/namespaceorg_1_1swallow__labs_1_1model_1_1_run_client" ],
+    [ "SocketClient", "d8/df8/namespaceorg_1_1swallow__labs_1_1model_1_1_socket_client.html", "d8/df8/namespaceorg_1_1swallow__labs_1_1model_1_1_socket_client" ]
+];
